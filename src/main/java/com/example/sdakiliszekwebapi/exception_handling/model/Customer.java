297e1b0aa7ee3d779aa9808631429f592e7115be
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-05 22:18:23 +0100

    Klasa modelowa

---
+++ src/main/java/com/example/sdakiliszekwebapi/exception_handling/model/Customer.java
@@ -0,0 +1,24 @@
+package com.example.sdakiliszekwebapi.exception_handling.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+
+}

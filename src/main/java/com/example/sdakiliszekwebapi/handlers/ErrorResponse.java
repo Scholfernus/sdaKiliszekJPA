201fcb0fe7aa92ba97a47179c3174e15b766c197
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-06 22:14:45 +0100

    2 klasy Exception nandling

---
+++ src/main/java/com/example/sdakiliszekwebapi/handlers/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.example.sdakiliszekwebapi.handlers;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private int statusCode;
+    private String message;
+}
---
+++ src/main/java/com/example/sdakiliszekwebapi/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.sdakiliszekwebapi.handlers;
+
+import com.example.sdakiliszekwebapi.exception.NoSuchCustomerExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value
+            = NoSuchCustomerExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody ErrorResponse handleException(NoSuchCustomerExistsException ex) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+    }
+
+    @ExceptionHandler(value
+            = MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody ErrorResponse handleException(MethodArgumentNotValidException ex) {
+        return new ErrorResponse(HttpStatus.NOT_ACCEPTABLE.value(), ex.getMessage());
+    }
+}

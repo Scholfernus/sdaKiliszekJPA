e66c80475968d7df3b089d689cf22f566dba633b
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-07 17:43:51 +0100

    Zmiany w repozytorium

--- src/main/java/com/example/sdakiliszekwebapi/repository/CustomerRepository.java
+++ src/main/java/com/example/sdakiliszekwebapi/repository/CustomerRepository.java
@@ -1,10 +1,10 @@
-package com.example.sdakiliszekwebapi.exception_handling.repository;
+package com.example.sdakiliszekwebapi.repository;
 
-import com.example.sdakiliszekwebapi.exception_handling.model.Customer;
+import com.example.sdakiliszekwebapi.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-boolean existsByName(String name);
+    boolean existsByName(String name);
 }

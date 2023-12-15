f40e30a44429e4bc1ff005e8580bb3c532548f17
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-15 20:27:15 +0100

    Usunąłem 1 folder z zadaniem 9 - blokował mi działający kod

--- src/main/java/com/example/sdakiliszekwebapi/zad9sda/exception/SdaException.java
+++
@@ -1,7 +0,0 @@
-package com.example.sdakiliszekwebapi.zad9sda.exception;
-
-public class SdaException extends RuntimeException {
-    public SdaException(final String message) {
-        super(message);
-    }
-}
